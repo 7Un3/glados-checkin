b292935e9b914b795848afef878b639388c0e042
7Un3 <133085102+7Un3@users.noreply.github.com>
2023-05-10 19:14:57 +0800

    Add files via upload

---
+++ glados_checkin.py
@@ -0,0 +1,180 @@
+
+import json
+
+import time
+
+import os
+
+import requests
+
+ 
+
+cookies = {
+
+ '_ga': 'GA1.1.494646603.1682950383',
+
+'_gid': 'GA1.2.1013999996.1683627762',
+
+'koa:sess':'eyJ1c2VySWQiOjMzNzU4MSwiX2V4cGlyZSI6MTcwODg3MDQxNzk0MSwiX21heEFnZSI6MjU5MjAwMDAwMDB9',
+
+'koa:sess.sig': 'pAEY-n6YoNNb2YrAXX7QWPDCMXQ',
+
+'Cookie.sig': '0.0.0',
+
+'Cookie': 'enabled',
+
+}
+
+ 
+
+_COOKIES = ';'.join(map(lambda x:'='.join(x),cookies.items()))
+
+_TOTAL_BUDGET = 5
+
+_PROXY = None
+
+ 
+
+def process(proxy = None) -> str:
+
+ _HOST = "glados.rocks"
+
+ _ORIGIN_URL =f"https://{_HOST}"
+
+ _USER_AGENT = "Mozilla/5.0(Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/91.0.4472.114 Safari/537.36"
+
+ 
+
+ checkin_url =f"{_ORIGIN_URL}/api/user/checkin"
+
+ status_url =f"{_ORIGIN_URL}/api/user/status"
+
+ referer_url =f"{_ORIGIN_URL}/console/checkin"
+
+ traffic_url =f"{_ORIGIN_URL}/api/user/traffic"
+
+ payload = {'token':'glados.network'}
+
+ 
+
+ checkin =requests.post(checkin_url,
+
+ headers={'cookie': _COOKIES,
+
+  'referer': referer_url,
+
+ 'origin':_ORIGIN_URL,
+
+ 'user-agent':_USER_AGENT,
+
+  'content-type':'application/json;charset=UTF-8'},
+
+ data=json.dumps(payload),
+
+ proxies=proxy)
+
+ msg = checkin.json()['message']
+
+ if msg =='\u6ca1\u6709\u6743\u9650':
+
+     return checkin.close() or'Failed due to expired cookies!'
+
+ 
+
+ status = requests.get(status_url,
+
+ headers={'cookie': _COOKIES,
+
+  'referer': referer_url,
+
+ 'origin': _ORIGIN_URL,
+
+ 'user-agent': _USER_AGENT},
+
+ proxies=proxy)
+
+ surplus =str(status.json()['data']['leftDays']).split('.')[0]
+
+ vip_level =status.json()['data']['vip']
+
+ 
+
+ traffic =requests.get(traffic_url,
+
+ headers={'cookie': _COOKIES,
+
+  'referer': referer_url,
+
+ 'origin':_ORIGIN_URL,
+
+ 'user-agent':_USER_AGENT,
+
+ 'content-type':'application/json;charset=UTF-8'},
+
+ proxies=proxy)
+
+ used_gb =traffic.json()["data"]["today"] / 1024**3
+
+ 
+
+ logMsg = (
+
+ '--------------------\n' +
+
+ 'GLaDOS CheckIn\n' +
+
+ 'Msg: ' + msg + '\n' +
+
+ 'Surplus: ' + surplus + 'Day\n' +
+
+ 'Usage: ' + '%.3f' % used_gb +
+
+ '/%.1f' % _TOTAL_BUDGET + 'GB\n' +
+
+ '--------------------'
+
+ )
+
+ checkin.close()
+
+ status.close()
+
+ traffic.close()
+
+ 
+
+ return logMsg
+
+ 
+
+ 
+
+timeOfDay = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+
+logFilePath = os.getcwd() + '/log.txt'
+
+logFile = open(logFilePath,mode='a')
+
+try:
+
+ logMsg = process()
+
+except:
+
+ try:logMsg = process(_PROXY)
+
+ except:logMsg = "Failed dueto network factors"
+
+writeMsg = (
+
+ '[' + timeOfDay + ']' + '\n' +
+
+ logMsg + '\n'
+
+ )
+
+logFile.write(writeMsg)
+
+print(writeMsg)
+
+logFile.close()
